ff0fa62901df41b8508807d6dbd4fedb6a2f2baf
L42ARO <alvaro512@usf.edu>
2024-01-23 03:15:50 -0500

    SLAM Mapping

---
+++ testing_dev/slam_tutorial/main.py
@@ -0,0 +1,28 @@
+import pygame
+import env, sensors
+import math
+
+environment = env.buildEnvironemnt((600, 1200))
+environment.originalMap = environment.map.copy()
+laser = sensors.LaserSensor(200, environment.originalMap, uncertainty=(0.7,0.1))
+environment.map.fill((0,0,0))
+environment.infomap = environment.map.copy()
+running = True
+
+while running:
+    sensorON = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if pygame.mouse.get_focused():
+            sensorON = True
+        elif not pygame.mouse.get_focused():
+            sensorON = False
+    if sensorON:
+        position=pygame.mouse.get_pos()
+        laser.position = position
+        sensor_data = laser.sense_obstables()
+        environment.dataStorage(sensor_data)
+        environment.show_sensorData()
+    environment.map.blit(environment.infomap, (0,0))
+    pygame.display.update()
---
+++ testing_dev/slam_tutorial/readme.md
@@ -0,0 +1,2 @@
+## Following this tutorial
+[SLAM Tutorial](https://www.youtube.com/watch?v=JbUNsYPJK1U&list=PL9RPomGb9IpRJLw5UTdSy4eJeoLrwNcfC&index=2)
